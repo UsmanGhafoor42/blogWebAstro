4b0c10d5a9aec6e1a0ee4493501bc031c48d1bc4
Usman Ghafoor <uk291244@gmail.com>
2024-09-18 09:58:36 -0700

    🚧 Work in progress.

---
+++ src/pages/api/test.ts
@@ -0,0 +1,16 @@
+export async function GET() {
+  return new Response(JSON.stringify({ msg: "Hello World" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+export async function POST() {
+  return new Response(JSON.stringify({ msg: "Hello World" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
