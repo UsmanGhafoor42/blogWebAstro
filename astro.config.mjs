ed08d0bb0c290ca3ea92bd6068e501d76a26c787
Usman Ghafoor <uk291244@gmail.com>
2024-09-19 03:13:25 -0700

    🚧 Work in progress.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "server",
+  adapter: vercel(),
 });
